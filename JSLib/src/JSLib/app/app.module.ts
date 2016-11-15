da05ff0de4340b18ed27acddd4c29fbb654d0e52
Larry Tenny <ltenny@gmail.com>
2016-11-15 10:56:26 -0600

    Added visual components

--- JSLib/src/JSLib/app/app.module.ts
+++ JSLib/src/JSLib/app/app.module.ts
@@ -2,10 +2,11 @@
 import { HttpModule, Http } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { VISUAL_COMPONENTS } from './visuals';
 
 @NgModule({
     imports: [BrowserModule, HttpModule],
-    declarations: [AppComponent],
+    declarations: [AppComponent, VISUAL_COMPONENTS],
     bootstrap: [AppComponent],
     entryComponents: [AppComponent]
 })
